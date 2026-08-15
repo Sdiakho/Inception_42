c481a05827899e10858bc2270bfe225da78dfd36
sdiakho <sdiakho@f1br3s6.paris.42.school>
2026-08-15 18:12:29 +0200

    chore: savegarde WIP nginx

---
+++ srcs/requirements/nginx/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -eu
+mkdir -p /var/www/html
+chown -R nginx:nginx /var/www/html
+if [  ! -f /etc/ssl/certs/nginx-selfsigned.crt ]; then
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=FR/ST=IDF/L=Paris/O=42/OU=42/CN=$DOMAIN_NAME/UID=$DB_USERNAME"
+fi
+
+exec nginx -g "daemon off;"
