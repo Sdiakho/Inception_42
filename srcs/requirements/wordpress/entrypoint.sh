003788215c313226b46ceb59eef47fdd9155869d
sdiakho <sdiakho@f1br3s6.paris.42.school>
2026-08-15 15:02:21 +0200

    feature: Wordpress: Dockerfile + entrypoint.sh

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -1,14 +1,14 @@
-#! bin/sh
+#!/bin/sh
 
 mkdir -p "/var/www/html"
-cd var/www/html
+cd /var/www/html
 
 if [ ! -f "wp-config.php" ]; then
 
 wp core download --allow-root
-wp config create --dbname=$DB_NAME --dbuser=$DB_USERNAME --dbpass=$DB_PASSWORD --allow-root
-wp core install --url=$DOMAIN_NAME --title=$TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --allow-root
-wp user create user1 user1@exemple.com --allow-root
+wp config create --dbname=$DB_NAME --dbuser=$DB_USERNAME --dbpass=$DB_PASSWORD --allow-root --dbhost=$DB_HOST --dbprefix=$DB_PREFIX
+wp core install --url=$DOMAIN_NAME --title=$TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root
+wp user create $USER1 $USER1_EMAIL --user_pass=$USER_PASSWORD --role=subscriber --allow-root
 
 fi
 
